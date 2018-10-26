0054aea4b8152020ab0f9371240ac83be41b0191
NeKoder <dimaannason@gmail.com>
2018-10-26 18:09:30 +0300

    First version + class drawing

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -22,8 +22,8 @@ public class Main extends Application {
         final Scene scene = new Scene(root, 1024, 768);
         scene.setFill(null);
         Labyrinth lab = new Labyrinth(10, 10);
-        drawLabyrinth(root, lab);
-        primaryStage.setTitle("JavaFX Line (o7planning.org)");
+        Drawing.drawLabyrinth(root, lab);
+        primaryStage.setTitle("");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -33,51 +33,5 @@ public class Main extends Application {
         launch(args);
     }
 
-    public static void drawLabyrinth(AnchorPane root, Labyrinth lab) {
-        Sector[][] sectors = lab.getSector();
-        LinkedList<Line> lines = new LinkedList<>();
-        Line oxLine1 = new Line(0, 0, 30, 0);
-        oxLine1.setStrokeWidth(1);
-        oxLine1.setStroke(Color.BLUE);
-        int o=50;
-        int cons=20;
-        int stena=70;
-        for (int i = 0; i < sectors.length; i++) {
-            for (int j = 0; j < sectors.length; j++) {
 
-
-
-                if(i==0&&j!=sectors[i].length-1)
-                    root.getChildren().add(new Line(i * o + cons, j * o + cons, i * o + cons, j * o + stena));// установка левой стенки сектора
-                else if(sectors[i][j].isUp()&&j!=sectors[i].length-1)
-                    if(!sectors[i-1][j].isDown())
-                        root.getChildren().add(new Line(i * o + cons, j * o + cons, i * o + cons, j * o + stena));
-
-
-
-                if(i==sectors.length-1&&j!=0)
-                    root.getChildren().add(new Line(i * o + stena, j * o + cons, i * o + stena, j * o + stena));// установка правой стенки сектора
-                else if (sectors[i][j].isDown()&&j!=0)
-                    if (!sectors[i + 1][j].isUp())
-                        root.getChildren().add(new Line(i * o + stena, j * o + cons, i * o + stena, j * o + stena));
-
-
-
-                if(j==0&&j!=sectors[i].length-1)
-                    root.getChildren().add(new Line(i * o + cons, j * o + cons, i * o + stena, j * o + cons));// установка левой стенки сектора
-
-                else if(sectors[i][j].isLeft()&&j!=sectors[i].length-1)
-                    if (!sectors[i][j-1].isRight())
-                        root.getChildren().add(new Line(i * o + cons, j * o + cons, i * o + stena, j * o + cons));
-
-
-                if(j==sectors[j].length-1&&j!=0)
-                    root.getChildren().add(new Line(i * o + cons, j * o + stena, i * o + stena, j * o + stena));//установка правой стенки сектора
-                else if(sectors[i][j].isRight()&&j!=0)
-                    if (!sectors[i][j+1].isLeft())
-                        root.getChildren().add(new Line(i * o + cons, j * o + stena, i * o + stena, j * o + stena));//
-
-            }
-        }
-    }
 }
