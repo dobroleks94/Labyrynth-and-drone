f9190ec46113441afd8ce92f25b43e034b5c890c
NeKoder <dimaannason@gmail.com>
2018-10-26 18:02:36 +0300

    First version + class drawing

--- src/sample/Drawing.java
+++ src/sample/Drawing.java
@@ -1,5 +1,13 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import java.awt.*;
+
 public class Drawing {
 
 }
